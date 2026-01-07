9185e6b1b0b8cee18305d023a6381db375dead25
haniehemat <hanihematasa@gmail.com>
2026-01-07 16:21:12 +0330

    html

---
+++ src/app/+shared/+base/base-crud-component/base-crud-component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-crud',
+  imports: [],
+  templateUrl: './base-crud-component.html',
+  styleUrl: './base-crud-component.scss',
+})
+export class BaseCrudComponent {
+
+}
